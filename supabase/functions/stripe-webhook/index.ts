79905d6dfe2f3880b324d17d866b63ddee338166
ICAZ117 <ibraheemcazalas@gmail.com>
2023-02-13 00:08:59 -0500

    Made edge function functional

--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -45,3 +45,7 @@ serve(async (request) => {
 //   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
 //   --header 'Content-Type: application/json' \
 //   --data '{"name":"Functions"}'
+
+// npx supabase functions serve --no-verify-jwt stripe-webhook --env-file ./supabase/functions/stripe-webhook/.env
+// stripe listen --forward-to localhost:54321/functions/v1/
+// stripe trigger payment_intent.succeeded
