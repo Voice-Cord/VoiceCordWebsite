cad5cbc69ec9a8f6432ff13e4be80eb3aa79ba6f
ICAZ117 <ibraheemcazalas@gmail.com>
2023-03-08 14:57:40 -0500

    Fixing ibiza's garbage code, as usual

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -5,7 +5,7 @@ export default {
 	async mounted() {
 		localStorage.removeItem("discord.accessToken");
 		localStorage.removeItem("discord.refreshToken");
-        localStorage.removeItem("discord.expiresIn");
+        localStorage.removeItem("discord.expiresAt");
 		localStorage.removeItem("discord.tokenType");
         this.$store.commit("setAuth", null);
         this.$router.push("/");
