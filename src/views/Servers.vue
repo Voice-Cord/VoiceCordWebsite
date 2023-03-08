3461942580f02c0ed495f1fb657b91987089c109
Abdurahman Tumgaev <abdurahman.tumgaev@gmail.com>
2023-03-08 03:59:23 +0100

    Servers get displayed

--- src/views/Servers.vue
+++ src/views/Servers.vue
@@ -4,17 +4,17 @@
 		<div class="nav-pad">
 			<div class="container my-5">
 				<h1 class="white center">Servers</h1>
-				<div class="row">
-					<div class="col-3-xl col-6-md col-12">
+				<div class="row gy-4">
+					<div v-for="guild in guilds" :key="guild" class="col-xl-3 col-md-6 col-12">
 						<div class="card" style="width: 18rem">
 							<img
 								class="card-img-top"
-								src="@/assets/img/PremiumFeatures/LongerMessages.png"
+								:src="guild.icon"
 								alt="Card image cap"
 							/>
 							<div class="card-body">
 								<p class="card-text" style="color: black">
-									Longer voice messages (up to 1 hour)
+                                    {{ guild.name }}
 								</p>
 							</div>
 						</div>
@@ -27,7 +27,39 @@
 
 <script>
 export default {
-    
+    data() {
+        return {
+            guilds: []
+        }
+    },
+    async mounted() {
+		const user = this.$store.getters.getAuth;
+        if(user) {
+            const item = {
+                method: "GET",
+                headers: {
+                    Authorization: 'Bearer ' + localStorage.getItem('discord.accessToken')
+                }
+            }
+
+            const res = await fetch('https://discord.com/api/users/@me/guilds', item).catch((e) => console.log(e));
+            if(!res) return;
+
+            const guilds = await res.json().catch((e) => console.log(e));
+            if(!guilds) return;
+
+            this.guilds = guilds;
+
+            console.log(guilds);
+            this.guilds.forEach((guild) => {
+                if(guild.icon) {
+                    guild.icon = `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`
+                } else {
+                    guild.icon = require('@/assets/img/Logo/TransparentBGFace.png')
+                }
+            });
+        }
+    }
 };
 </script>
 
