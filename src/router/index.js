460310899b2031980e54df51a99bb01f6928cf7d
ICAZ117 <ibraheemcazalas@gmail.com>
2023-03-09 19:36:01 -0500

    Fixed router issue

--- src/router/index.js
+++ src/router/index.js
@@ -5,74 +5,75 @@ import NotFound from "../views/NotFound.vue";
 import { authService } from "../classes/authService.js";
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/support",
-    name: "Support",
-    component: () => import("../views/Support.vue"),
-  },
-  {
-    path: "/documentation",
-    name: "Documentation",
-    component: () => import("../views/Docs.vue"),
-  },
-  {
-    path: "/invite",
-    name: "Invite",
-    component: () => import("../views/Invite.vue"),
-  },
-  {
-    path: "/upgrade",
-    name: "Upgrade",
-    component: () => import("../views/Upgrade.vue"),
-    meta: { requiresAuth: true },
-  },
-  {
-    path: "/servers",
-    name: "Servers",
-    component: () => import("../views/Servers.vue"),
-    meta: { requiresAuth: true },
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: () => import("../views/Login.vue"),
-  },
-  {
-    path: "/logout",
-    name: "Logout",
-    component: Logout,
-  },
-  {
-    path: "/:pathMatch(.*)",
-    name: "NotFound",
-    component: NotFound,
-  },
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/support",
+        name: "Support",
+        component: () => import("../views/Support.vue"),
+    },
+    {
+        path: "/documentation",
+        name: "Documentation",
+        component: () => import("../views/Docs.vue"),
+    },
+    {
+        path: "/invite",
+        name: "Invite",
+        component: () => import("../views/Invite.vue"),
+    },
+    {
+        path: "/upgrade",
+        name: "Upgrade",
+        component: () => import("../views/Upgrade.vue"),
+        meta: { requiresAuth: true },
+    },
+    {
+        path: "/servers",
+        name: "Servers",
+        component: () => import("../views/Servers.vue"),
+        meta: { requiresAuth: true },
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: () => import("../views/Login.vue"),
+    },
+    {
+        path: "/logout",
+        name: "Logout",
+        component: Logout,
+    },
+    {
+        path: "/:pathMatch(.*)",
+        name: "NotFound",
+        component: NotFound,
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
-const isLoggedIn = authService.isLoggedIn();
-
 router.beforeEach((to, from) => {
-  // instead of having to check every route record with
-  // to.matched.some(record => record.meta.requiresAuth)
-  if (to.meta.requiresAuth && !isLoggedIn) {
-    // this route requires auth, check if logged in
-    // if not, redirect to login page.
-    return {
-      path: "/login",
-      // save the location we were at to come back later
-      query: { redirect: to.fullPath },
-    };
-  }
+    const isLoggedIn = authService.isLoggedIn();
+    console.log("ROUTER LOGIN STATE:", isLoggedIn);
+
+    // instead of having to check every route record with
+    // to.matched.some(record => record.meta.requiresAuth)
+    if (to.meta.requiresAuth && !isLoggedIn) {
+        // this route requires auth, check if logged in
+        // if not, redirect to login page.
+        return {
+            path: "/login",
+            // save the location we were at to come back later
+            query: { redirect: to.fullPath },
+        };
+    }
 });
 
 export default router;
